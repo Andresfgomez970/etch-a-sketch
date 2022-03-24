e05517d79e954471b88d0587bf432c1ecc38833f
andresg <Andres Gomez>
2022-03-24 15:23:50 -0500

    adding grey function, clean, and setting up other aesthetic things

--- code.js
+++ code.js
@@ -6,6 +6,8 @@ color_state = 0;
 jamboardState = 0;
 clickState = 0;
 permanentState = 0;
+eraserState = 0;
+greyState = 0;
 
 function updateJamboardState(){
     jamboardState = !jamboardState;
@@ -19,26 +21,44 @@ function updatePermanentState(){
     permanentState = !permanentState;
 }
 
+function updateEraserState(){
+    eraserState = !eraserState;
+}
+
+
 // Indert just after the element of interest
 function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
-function hoverEffect(event, time=500){
-    if (time < Infinity && ~event.target.classList.toString().includes("hover-effect") ){
-        event.target.style["background-color"] = "orange";
-        
-        // event.target.classList.add("hover-effect");
+function hoverEffect(event, time=500, color="orange"){
+    
+    if (time < Infinity){
+        // Get actual color before transition
+        if (!event.target.classList.toString().includes("transition")){
+            if (event.target.style["background-color"] == ""){
+                event.target.setAttribute("color-before", "white");
+            }
+            else{
+                event.target.setAttribute("color-before", String(event.target.style["background-color"]));
+            }
+            event.target.style["background-color"] = color;
+            event.target.classList.add("transition");
+        }
+
+        // Do transition and go to original color
+        if (event.target.classList.toString().includes("transition")){
+            setTimeout(function() {
+                event.target.style["background-color"] = event.target.getAttribute("color-before"); 
+                event.target.classList.remove("transition");
+            }, time);
+        }
+    }
+    
+    else{
+        event.target.style["background-color"] = color;
     }
 
-    if (time < Infinity){
-        setTimeout(function() {
-            event.target.classList.remove("hover-effect"); 
-        }, time);
-    } 
-    else{
-        event.target.classList.add("permanent-effect");
-    }
 
 }
 
@@ -47,83 +67,184 @@ function randomIntFromInterval(min, max) { // min and max included
 }
 
 
-function rainbowEffect(event){
+function rainbowEffect(event, time=500){
     let min = 0; let max = 259.9999;
     let r = randomIntFromInterval(min, max);
     let g = randomIntFromInterval(min, max);
     let b = randomIntFromInterval(min, max);
 
+    if (time < Infinity){
+        // Get actual color before transition
+        if (!event.target.classList.toString().includes("transition")){
+            if (event.target.style["background-color"] == ""){
+                event.target.setAttribute("color-before", "white");
+            }
+            else{
+                event.target.setAttribute("color-before", String(event.target.style["background-color"]));
+            }
+            event.target.style["background-color"] = "rgb("+r+","+g+","+b+")";
+            event.target.classList.add("transition");
+        }
 
-    // if (event.target.style["background-color"] == ""){
-    //     if (event.target.classList.toString().includes("permanent-effect")){
-    //         color_before = "orange";
-    //     }
-    //     else{
-    //         color_before = "white";
-    //     }
-    // }
+        // Do transition and go to original color
+        if (event.target.classList.toString().includes("transition")){
+            setTimeout(function() {
+                event.target.style["background-color"] = event.target.getAttribute("color-before"); 
+                event.target.classList.remove("transition");
+            }, time);
+        }
+    }
+    
+    else{
+        event.target.style["background-color"] = "rgb("+r+","+g+","+b+")";
+    }
 
-    // console.log(event.target.style["background-color"]);
-    // if (time < Infinity && ~event.target.classList.toString().includes("hover-effect") ){
-    //     event.target.classList.add("hover-effect");
-    // }
+}
 
-    // if (time < Infinity){
-    //     setTimeout(function() {
-    //         event.target.classList.remove("hover-effect"); 
-    //     }, time);
-    // } 
-    // else{
-    //     event.target.classList.add("permanent-effect");
-    // }
+function greyEffect(event, time=500){
+    let step = 254.99999 * 0.1;
+    let result;
+    let rgb = "rgb(255, 255, 255)";
+    
+    if (time < Infinity){
+        // Get actual color before transition
+        if (!event.target.classList.toString().includes("transition-grey")){
+            if (event.target.style["background-color"] == ""){
+                event.target.setAttribute("color-before", "rgb("+255+","+255+","+255+")");
+                rgb = event.target.getAttribute("color-before"); 
+            }
+            else{
+                event.target.setAttribute("color-before", String(event.target.style["background-color"]));
+
+                if (event.target.getAttribute("color-before").substring(0, 3) != "rgb"){
+                    rgb = "rgb("+255+","+255+","+255+")";
+                }
+            }
+
+            rgb = event.target.getAttribute("color-before").substring(4, rgb.length-1).replace(")", "").replace(/ /g, '').split(',');
+
+            if (rgb[0] == rgb[1] && rgb[1] == rgb[2]){
+                result = +rgb[0] - step;
+            }
+            else{
+                result = 255 - step;
+            }
+
+            event.target.style["background-color"] = "rgb("+result+","+result+","+result+")";
+            event.target.setAttribute("color-grey", String(event.target.style["background-color"]));
+            event.target.classList.add("transition-grey");
+        }
+
+        // Do transition and go to original color
+        if (event.target.classList.toString().includes("transition-grey")){
+
+            rgb = event.target.getAttribute("color-grey").substring(4, rgb.length-1).replace(")", "").replace(/ /g, '').split(',');
+
+            if (rgb[0] == rgb[1] && rgb[1] == rgb[2]){
+                result = +rgb[0] - step;
+            }
+            else{
+                result = 255 - step;
+            }
+
+            event.target.style["background-color"] = "rgb("+result+","+result+","+result+")";
+            event.target.setAttribute("color-grey", String(event.target.style["background-color"]));
+
+            setTimeout(function() {
+                event.target.style["background-color"] = event.target.getAttribute("color-before"); 
+                event.target.classList.remove("transition-grey");
+            }, time);
+        }
+    }
+    
+    else{
+        if (event.target.getAttribute("color-grey") == ""){
+            result = 255 - step;
+            rgb =  "rgb("+result+","+result+","+result+")";
+        }
+        else if (event.target.classList.toString().includes("transition-grey")){
+            result = 255 - step;
+            rgb =  "rgb("+result+","+result+","+result+")";
+        }
+        else{
+            rgb = event.target.getAttribute("color-grey").substring(4, rgb.length-1).replace(")", "").replace(/ /g, '').split(',');
+
+            if (rgb[0] == rgb[1] && rgb[1] == rgb[2]){
+                result = +rgb[0] - step;
+            }
+            else{
+                result = 255 - step;
+            }
+
+            rgb =  "rgb("+result+","+result+","+result+")";
+        }
 
 
-    event.target.style["background-color"] = "rgb("+r+","+g+","+b+")";
-    setTimeout(function() {
-        event.target.style["background-color"]  = color_before;
-    }, 500);
+        event.target.style["background-color"] = rgb;
+        event.target.setAttribute("color-grey", String(event.target.style["background-color"]));
+    }
 
 
 }
 
 
-
 function changeColor(event){
     
-    // default hover writing
-    if (jamboardState == 0 && permanentState == 0){
+    if (eraserState){
+        return hoverEffect(event, Infinity, "white");
+    }
+
+    // hover, not permanent
+    else if (jamboardState == 0 && permanentState == 0){
         if (color_state == RAINBOW_STATE){
             return rainbowEffect(event);
+        }
+        else if (color_state == GREY_STATE){
+            return greyEffect(event, 5000);
         }
         else{
             return hoverEffect(event);
         }
     }
 
-    // default hover writing, but permanent
+    // hover, but permanent
     else if (jamboardState == 0 && permanentState){
         if (color_state == RAINBOW_STATE){
-            return rainbowEffect(event);
+            return rainbowEffect(event, Infinity);
+        }
+        else if (color_state == GREY_STATE){
+            return greyEffect(event, Infinity);
         }
         else{
             return hoverEffect(event, Infinity);
         }
     }
 
-    // default jamboard writing but not permanent 
-    else if (color_state == DEFAULT_STATE && jamboardState && clickState && permanentState == 0){
-        return hoverEffect(event);
+    // jamboard, not permanent 
+    else if (jamboardState && clickState && permanentState == 0){
+        if (color_state == RAINBOW_STATE){
+            return rainbowEffect(event);
+        }
+        else if (color_state == GREY_STATE){
+            return greyEffect(event, 5000);
+        }
+        else{
+            return hoverEffect(event);
+        }    
     }
 
-    // jamboard writing, but permanent
-    else if (color_state == DEFAULT_STATE && jamboardState && clickState && permanentState){
-        return hoverEffect(event, Infinity);
+    // jamboard, but permanent
+    else if (jamboardState && clickState && permanentState){
+        if (color_state == RAINBOW_STATE){
+            return rainbowEffect(event, Infinity);
+        }
+        else if (color_state == GREY_STATE){
+            return greyEffect(event, Infinity);
+        }
+        else{
+            return hoverEffect(event, Infinity);
+        }
     }
-
-
-    // else if (color_state == RAINBOW_STATE && jamboardState && clickState){
-    //     return rainbowEffect(event);
-    // }
 
 
 }
@@ -162,14 +283,29 @@ function createGridElements(numberOfElements=16){
     rainbowModeButton.addEventListener("click", activateRainbowMode);    
     activateRainbowMode.buttonState = 0;
 
+    greyModeButton = document.querySelector(names[2]);
+    greyModeButton.addEventListener("click", activateGreyMode);    
+    activateGreyMode.buttonState = 0;
+
     permanentModeButton = document.querySelector(names[3]);
     permanentModeButton.addEventListener("click", activatePermanentMode);    
     activatePermanentMode.buttonState = 0;
+
+    eraserModeButton = document.querySelector(names[4]);
+    eraserModeButton.addEventListener("click", activateEraserMode);    
+    activateEraserMode.buttonState = 0;
+
+    cleanModeButton = document.querySelector(names[5]);
+    cleanModeButton.addEventListener("click", activateCleanMode);    
+    activateCleanMode.buttonState = 0;
+
 
     color_state = 0;
     jamboardState = 0;
     clickState = 0;
     permanentState = 0;
+    eraserState = 0;
+    greyState = 0;
 
     // create and add principal container of the pad grid, just after the header
     pad = document.createElement("div");
@@ -187,6 +323,8 @@ function createGridElements(numberOfElements=16){
             pad_element.classList.add("pad-element");
             pad_element.style["min-width"] = min_width + "px"; 
             pad_element.style["min-height"] = min_width + "px";
+            pad_element.setAttribute("color-before", "");
+            pad_element.setAttribute("color-grey", "");
             pad_line.append(pad_element);
         }
     }
@@ -200,7 +338,8 @@ function createGridElements(numberOfElements=16){
 
 
 // create all the elements inside the path with the input of the user 
-createGridElements(10)
+numberPerSide = 10;
+createGridElements(numberPerSide)
 
 
 // listen to mouse movement just after creating new blank pad
@@ -233,13 +372,15 @@ function activateJamboardMode(){
     if (activateJamboardMode.buttonState){
         jamboardModeButton.classList.add("pressed-button");
 
-        pad_elements = document.querySelectorAll(".pad-element");
-        pad_elements.forEach(element => {
+        html_elements = document.querySelectorAll("html");
+        
+        html_elements.forEach(element => {
             // add new listaners
             element.addEventListener("mousedown", updateClickState);
             element.addEventListener("mouseup", updateClickState);
         });
     }
+
     else{
         jamboardModeButton.classList.remove("pressed-button");
     }
@@ -253,6 +394,10 @@ function activateRainbowMode(){
     activateRainbowMode.buttonState = ~activateRainbowMode.buttonState;
 
     if (activateRainbowMode.buttonState){
+        if (activateGreyMode.buttonState){
+            activateGreyMode();
+        }
+
         color_state = RAINBOW_STATE;
         rainbowModeButton.classList.add("pressed-button");
     }
@@ -262,7 +407,29 @@ function activateRainbowMode(){
     }
 }
 
+
+greyModeButton = document.querySelector(names[2]);
+
+function activateGreyMode(){
+    activateGreyMode.buttonState = ~activateGreyMode.buttonState;
+
+    if (activateGreyMode.buttonState){
+        if (activateRainbowMode.buttonState){
+            activateRainbowMode();
+        }
+
+        color_state= GREY_STATE;
+        greyModeButton.classList.add("pressed-button");
+    }
+    else{
+        color_state = DEFAULT_STATE;
+        greyModeButton.classList.remove("pressed-button");
+    }
+}
+
+
 permanentModeButton = document.querySelector(names[3]);
+
 function activatePermanentMode(){
     activatePermanentMode.buttonState = ~activatePermanentMode.buttonState;
     updatePermanentState();
@@ -274,5 +441,40 @@ function activatePermanentMode(){
         permanentModeButton.classList.remove("pressed-button");
     }
 }
+
+eraserModeButton = document.querySelector(names[4]);
+
+function activateEraserMode(){
+    activateEraserMode.buttonState = ~activateEraserMode.buttonState;
+    updateEraserState();
+
+    if (activateEraserMode.buttonState){
+        eraserModeButton.classList.add("pressed-button");
+    }
+    else{
+        eraserModeButton.classList.remove("pressed-button");
+    }
+}
+
+
+cleanModeButton = document.querySelector(names[5]);
+
+function activateCleanMode(){
+    activateCleanMode.buttonState = ~activateCleanMode.buttonState;
+
+    pad = document.querySelector(".content"); 
+    content.remove();
+    createGridElements(numberPerSide);
+    
+    if (activateCleanMode.buttonState){
+        cleanModeButton.classList.add("pressed-button");
+    }
+    else{
+        cleanModeButton.classList.remove("pressed-button");
+    }
+}
+
+
+
 
 
